3670a2f0366461b6417ec57c26f659e89a3ea8b5
Dwight Gunning <dgunning@gmail.com>
2021-11-30 20:41:48 -0500

    Add ngrams function

--- src/opencanada/utils.py
+++ src/opencanada/utils.py
@@ -1,4 +1,5 @@
 import rich
+import re
 
 LANGUAGES = {
     'en': 'en',
@@ -7,7 +8,6 @@ LANGUAGES = {
     'french': 'fr',
     'francais': 'fr',
     'anglais': 'en'
-
 }
 
 
@@ -27,3 +27,29 @@ def dirprint(obj: object):
     rich.print('--------------------')
     rich.print('\n'.join(sorted([a for a in dir(obj)
                                  if not a.startswith('_')])))
+
+
+def generate_ngrams(s, n):
+    # Convert to lowercases
+    s = s.lower()
+
+    # Replace all none alphanumeric characters with spaces
+    s = re.sub(r'[^a-zA-Z0-9\s]', ' ', s)
+
+    # Break sentence in the token, remove empty tokens
+    tokens = [token for token in s.split(" ") if token != ""]
+
+    # Use the zip function to help us generate n-grams
+    # Concatentate the tokens into ngrams and return
+    ngrams = zip(*[tokens[i:] for i in range(n)])
+    return [" ".join(ngram) for ngram in ngrams]
+
+
+if __name__ == '__main__':
+    s = """
+        Natural-language processing (NLP) is an area of
+        computer science and artificial intelligence
+        concerned with the interactions between computers
+        and human (natural) languages.
+    """.strip()
+    print(generate_ngrams(s, 2))
